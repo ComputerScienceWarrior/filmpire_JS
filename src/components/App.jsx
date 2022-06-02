783c98195bd0ef20c678f96d6c7b90d7115469c5
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-06-01 20:18:35 -0700

    add => Various components to app and import useStyles() hook from material UI.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Route, Switch } from 'react-router-dom'
+import { Movies, MovieInformation, Actors, Profile, Navbar } from '../components'
+
+import useStyles from './Styles'
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>App</div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar}>
+
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
   )
 }
 
